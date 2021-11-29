6ba7f407b28167099362a10507c10daa63bdcf56
Werbert Viana <werbertbrito@gmail.com>
2021-11-29 11:53:19 -0300

    criacao tabela content

--- src/shared/typeorm/migrations/1638197238797-CreateContent.ts
+++ src/shared/typeorm/migrations/1638197238797-CreateContent.ts
@@ -1,11 +1,51 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateContent1638197238797 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'content',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'image',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'music',
+                        type: 'varchar',
+                        isNullable: true
+                    },
+                    {
+                        name: 'video',
+                        type: 'varchar',
+                        isNullable: true
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp with time zone',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp with time zone',
+                        default: 'now()',
+                    }
+                ]
+
+            })
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('content');
     }
 
 }
